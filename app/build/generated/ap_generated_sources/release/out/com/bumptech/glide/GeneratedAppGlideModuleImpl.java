f7d01bfceee8d9800ba9f6adcfad3edf516a993b
appswaakan <apps@waakan.com>
2020-01-21 18:57:20 +0530

    21jan19 waakan hgd unversioned files commit

---
+++ app/build/generated/ap_generated_sources/release/out/com/bumptech/glide/GeneratedAppGlideModuleImpl.java
@@ -0,0 +1,51 @@
+package com.bumptech.glide;
+
+import android.content.Context;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule;
+import com.mealarts.Helpers.CustomAppGlideModule;
+import java.util.Collections;
+import java.util.Set;
+
+@SuppressWarnings("deprecation")
+final class GeneratedAppGlideModuleImpl extends GeneratedAppGlideModule {
+  private final CustomAppGlideModule appGlideModule;
+
+  public GeneratedAppGlideModuleImpl(Context context) {
+    appGlideModule = new CustomAppGlideModule();
+    if (Log.isLoggable("Glide", Log.DEBUG)) {
+      Log.d("Glide", "Discovered AppGlideModule from annotation: com.mealarts.Helpers.CustomAppGlideModule");
+      Log.d("Glide", "Discovered LibraryGlideModule from annotation: com.bumptech.glide.integration.okhttp3.OkHttpLibraryGlideModule");
+    }
+  }
+
+  @Override
+  public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
+    appGlideModule.applyOptions(context, builder);
+  }
+
+  @Override
+  public void registerComponents(@NonNull Context context, @NonNull Glide glide,
+      @NonNull Registry registry) {
+    new OkHttpLibraryGlideModule().registerComponents(context, glide, registry);
+    appGlideModule.registerComponents(context, glide, registry);
+  }
+
+  @Override
+  public boolean isManifestParsingEnabled() {
+    return appGlideModule.isManifestParsingEnabled();
+  }
+
+  @Override
+  @NonNull
+  public Set<Class<?>> getExcludedModuleClasses() {
+    return Collections.emptySet();
+  }
+
+  @Override
+  @NonNull
+  GeneratedRequestManagerFactory getRequestManagerFactory() {
+    return new GeneratedRequestManagerFactory();
+  }
+}
